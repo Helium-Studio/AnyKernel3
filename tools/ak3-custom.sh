c2d0c0a6297578d56a2b84e0d0cc93a13d17fdca
Helium-Studio <67852324+Helium-Studio@users.noreply.github.com>
2025-03-17 02:09:20 +0800

    AK3: Clean cache directories after installation

--- tools/ak3-custom.sh
+++ tools/ak3-custom.sh
@@ -23,3 +23,11 @@ patch_dtb() {
   cat $AKHOME/Image.gz $AKHOME/dtb > $AKHOME/Image.gz-dtb;
   rm -f $AKHOME/Image.gz $AKHOME/dtb;
 }
+
+clean_cache() {
+  ui_print " " "Installation done. Cleaning cache...";
+  rm -rf /cache/*;
+  rm -rf /data/dalvik-cache;
+  rm -rf /data/resource-cache;
+  rm -rf /data/system/package_cache;
+}
